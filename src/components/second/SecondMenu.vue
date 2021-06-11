2cda2c2022b907847fb47a83788631659d5bee5b
whiscovery <whiscovery@gmail.com>
2021-06-11 16:46:39 +0900

    .

--- src/components/second/SecondMenu.vue
+++ src/components/second/SecondMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="d-grid gap-2 d-md-block mt-3 mb-3">
+    <div class="mt-3 mb-3">
   <router-link to="/secondfood/list"><button class="btn btn-yellow" type="button">목록</button></router-link>
   <router-link to="/secondfood/secondmap"><button class="btn btn-yellow ms-2" type="button">지도</button></router-link>
   <router-link to="/secondfood/insert"><button class="btn btn-yellow ms-2" type="button">후보지입력</button></router-link>
--- src/views/SecondFood.vue
+++ src/views/SecondFood.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-      <div class="alert alert-primary alert-small" role="alert">
-  일단 가보고 맛집인지 판단할 곳, 여행 계획 세울 때 사용하세요. 귀찮으니 로그인같은거 없어요.
+      <div class="alert alert-primary alert-small alert-dismissible fade show" role="alert">
+  일단 가보고 맛집인지 판단할 곳, 여행 계획 세울 때 사용하세요.<br />
+  +를 누르면 후보지에서 맛집으로 올라가고, x를 누르면 삭제됩니다.<br />
+  남의 기록 지우지 않게 조심!
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
       <SecondMenu />
       <div>
